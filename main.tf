c5d8fb6bee58a226355cdd494edf181ac901e7ff
Abhishek-EN <awork478@gmail.com>
2023-04-11 00:07:20 +0530

    adding comments

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_spot_fleet_request" "SPOT" {
-  iam_fleet_role                = "arn:aws:iam::1234567890:role/aws-ec2-spot-fleet-tagging-role"
+  iam_fleet_role                = "arn:aws:iam::1234567890:role/aws-ec2-spot-fleet-tagging-role" # Your spot fleet role arn
   target_capacity               = 1
   allocation_strategy           = "lowestPrice"
   wait_for_fulfillment          = true
@@ -7,8 +7,8 @@ resource "aws_spot_fleet_request" "SPOT" {
 
   launch_template_config {
     launch_template_specification {
-      id      = "lt-123567890"
-      version = "1"
+      id      = "lt-123567890" # Your Launch template ID
+      version = "1" # Launch template version
     }
   }
 }
